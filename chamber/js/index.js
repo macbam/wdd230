262c776c6423c9e7efec3f124dabe89adf4c8220
macbam <macbam1108@gmail.com>
2022-02-13 22:06:10 -0700

    W06-01

--- chamber/js/index.js
+++ chamber/js/index.js
@@ -35,3 +35,32 @@ currentYearjs.textContent = currentYear.toString();
 document.querySelector(
 	"#lastModified"
 ).textContent = `Last Modification: ${document.lastModified}`;
+
+
+// WIND CHILL
+
+function f(c) {
+    return (c * (9 / 5)) + 32;
+}
+
+function c(f) {
+    return (f - 32) * (5 / 9);
+}
+
+function mph(kph) {
+    return kph * 0.621371;
+}
+
+const windSpeedKPH = document.querySelector('.windSpeed');
+
+const temp = f(33);
+const windSpeed = mph(windSpeedKPH);
+
+var windChill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(windSpeed,0.16)) + (0.4275*temp*Math.pow(windSpeed,0.16));
+
+var windChill = c(windChill); 
+
+// console.log(windChill);
+windChill = Math.round(windChill);
+// console.log(windChill);
+document.getElementById("windChill").innerHTML = windChill;
